b818210e8a68bd0439df7b4f21b30e7eea67faef
Radost09 <Djudji83@gmail.com>
2022-06-16 15:48:24 +0300

    100/100

---
+++ SoftUni Parking-Skeleton/SoftUniParking/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.HorsePower = horsePower;
+            this.RegistrationNumber = registrationNumber;
+        }
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int HorsePower { get { return horsePower; } set { horsePower = value; } }
+        public string RegistrationNumber { get { return registrationNumber; } set { registrationNumber = value; } }
+
+        public static Car First { get; internal set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Make: {Make}\n");
+            sb.Append($"Model: {Model}\n");
+            sb.Append($"HorsePower: {HorsePower}\n");
+            sb.Append($"RegistrationNumber: {RegistrationNumber}");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ SoftUni Parking-Skeleton/SoftUniParking/Parking.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        public Parking(int capacity)
+        {
+            this.Cars = new List<Car>();
+            this.Capacity = capacity;
+        }
+        private List<Car> cars;
+        private int capacity;
+
+        public List<Car> Cars { get { return cars; } set { cars = value; } }
+        public int Capacity { get { return capacity; } set { capacity = value; } }
+        public int Count { get { return Cars.Count; } }
+
+        public string AddCar(Car addedCar)
+        {
+            bool canAddCar = true;
+            foreach (Car car in cars)
+            {
+                if(car.RegistrationNumber == addedCar.RegistrationNumber)
+                {
+                    canAddCar = false;
+                    return "Car with that registration number, already exists!";
+                    
+                }
+
+            }
+            if(Cars.Count + 1 > Capacity)
+            {
+                canAddCar = false;
+                return "Parking is full!";
+            }
+            if(canAddCar)
+            {
+                Cars.Add(addedCar);
+                return $"Successfully added new car {addedCar.Make} {addedCar.RegistrationNumber}";
+            }
+            return "";
+        }
+        public string RemoveCar(string registrationNumber)
+        {
+            bool isAvailable = false;
+            foreach (Car car in Cars)
+            {
+                if(car.RegistrationNumber == registrationNumber)
+                {
+                    isAvailable = true;
+                }
+            }
+            if(!isAvailable)
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+            else
+            {
+                Car carToRemove = Cars.First(c => c.RegistrationNumber == registrationNumber);
+                Cars.Remove(carToRemove);
+                return $"Successfully removed {registrationNumber}";
+            }
+        }
+        public Car GetCar(string registrationNumber)
+        {
+            return Cars.First(c => c.RegistrationNumber == registrationNumber);
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> registrationNumbers)
+        {
+            foreach (string registrationNumber in registrationNumbers)
+            {
+                RemoveCar(registrationNumber);
+            }
+        }
+    }
+}
